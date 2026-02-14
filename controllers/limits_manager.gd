5964960ead12048265e28bc997588271b6ce5613
Ethan <157554972+ethanp4@users.noreply.github.com>
2026-02-14 16:49:04 -0700

    add jump limit, manager, and its ui

---
+++ controllers/limits_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var _jumps_remaining: int = 3
+
+func _ready() -> void:
+	ManagerManager.limits_manager = self
+
+func can_jump() -> int:
+	return get_jumps() != 0
+
+func get_jumps() -> int:
+	return _jumps_remaining
+	
+func use_jump() -> void:
+	_jumps_remaining -= 1
